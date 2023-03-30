a75507e90c1aedd35b62d07177f982a41a68c8c1
Jamal Haider <77342903+jamal-haider@users.noreply.github.com>
2023-03-30 15:33:31 +0500

    gifs only checkbox

--- index.js
+++ index.js
@@ -1,32 +1,63 @@
 import { catsData } from './data.js'
 let emotionRadios = document.getElementById('emotion-radios')
 const getImageBtn = document.getElementById('get-image-btn')
+let memeModalInner = document.getElementById('meme-modal-inner')
+let memeModal = document.getElementById('meme-modal')
+let memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
+let gifsOnlyOption = document.getElementById('gifs-only-option')
 
+memeModalCloseBtn.addEventListener('click', function(){
+    memeModal.style.display = 'none'
+})
 
 getImageBtn.addEventListener('click', renderCat)
 
 function renderCat(){
     const catObject = getSingleCatObject()
-    console.log(catObject)
+    memeModalInner.innerHTML = `
+    <img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+    >
+    `
+    memeModal.style.display = 'flex'
 }
 
 function getSingleCatObject(){
-    const emotion = document.querySelector('input[type="radio"]:checked').value
-    const catArray = []
-    for(let cat of catsData)
-        if(cat.emotionTags.includes(emotion))
-            catArray.push(cat)
-    const randomNumber = ~~(Math.random() * catArray.length)
-    return catArray[randomNumber]
+
+    if(document.querySelector('input[type="radio"]:checked')){
+        const emotion = document.querySelector('input[type="radio"]:checked').value
+        const matchingCatsArray = catsData.filter(function(cat){
+            if(gifsOnlyOption.checked){
+                return cat.emotionTags.includes(emotion) && cat.isGif
+            }
+            else{
+                return cat.emotionTags.includes(emotion)
+            }
+        })
+            
+        if(matchingCatsArray.length === 1){
+            return matchingCatsArray[0]
+        }
+        else if(matchingCatsArray.length > 1){
+            const randomNumber = ~~(Math.random() * matchingCatsArray.length)
+            return matchingCatsArray[randomNumber]
+        }
+    
+    }
+    
 }
 
 emotionRadios.addEventListener('click', highlightCheckedOption)
 
 function highlightCheckedOption(e){
     const radios = document.getElementsByClassName('radio') 
-    for(let radio of radios)
+    for(let radio of radios){
         radio.classList.remove('highlight')
+    }
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
+
 }
 
 function getEmotionsArray(cats){
